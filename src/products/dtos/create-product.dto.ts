54a0b103a45c264af9fc5ac1a55070574ac5a098
Nuttavut Thongjor <n.thongjor@gmail.com>
2024-09-09 17:09:05 +0700

    add ability to validate product DTO

--- src/products/dtos/create-product.dto.ts
+++ src/products/dtos/create-product.dto.ts
@@ -1,6 +1,25 @@
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNumber,
+  IsString,
+  Length,
+} from 'class-validator';
+
 export class CreateProductDto {
+  @IsString()
+  @Length(5, 50)
   name: string;
+
+  @IsString()
+  @Length(10, 150)
   desc: string;
+
+  @IsNumber()
   price: number;
+
+  @IsArray()
+  @IsNumber({}, { each: true })
+  @ArrayMinSize(1)
   categoryIds: number[];
 }
--- src/products/dtos/find-all-query.dto.ts
+++ src/products/dtos/find-all-query.dto.ts
@@ -1,4 +1,14 @@
+import { Transform } from 'class-transformer';
+import { IsNumber, IsOptional } from 'class-validator';
+
 export class FindAllQueryDto {
+  @IsNumber()
+  @IsOptional()
+  @Transform(({ value }) => +value)
   page?: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Transform(({ value }) => +value)
   limit?: number;
 }
